1fddcbdf29afc5bb33dcec6756d3e5bc9ded4293
Tiresa Dombrowski <35544695+tjdombrowski@users.noreply.github.com>
2019-04-24 14:45:51 -0500

    Improved badge display

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,6 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 @NgModule({
   declarations: [
     AppComponent
